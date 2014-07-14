018a1b311b2dcbf415db8a0f418fa01e66feee0f
Cory Thomas <cory.thomas@bazaarvoice.com>
2014-07-14 11:07:09 -0500

    Renamed setter to be consistent

--- memcached/src/main/java/com/bazaarvoice/dropwizard/caching/memcached/MemcachedResponseStoreFactory.java
+++ memcached/src/main/java/com/bazaarvoice/dropwizard/caching/memcached/MemcachedResponseStoreFactory.java
@@ -13,6 +13,7 @@ import net.spy.memcached.MemcachedClient;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Arrays;
 import java.util.List;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -49,18 +50,9 @@ public class MemcachedResponseStoreFactory implements ResponseStoreFactory {
         return _servers;
     }
 
-    public MemcachedResponseStoreFactory servers(List<InetSocketAddress> servers) {
+    public void setServers(List<InetSocketAddress> servers) {
         checkNotNull(servers);
-        checkArgument(servers.size() > 0, "at least one server address is required");
         _servers = ImmutableList.copyOf(servers);
-        return this;
-    }
-
-    public MemcachedResponseStoreFactory servers(InetSocketAddress... servers) {
-        checkNotNull(servers);
-        checkArgument(servers.length > 0, "at least one server address is required");
-        _servers = ImmutableList.copyOf(servers);
-        return this;
     }
 
     /**
@@ -69,9 +61,11 @@ public class MemcachedResponseStoreFactory implements ResponseStoreFactory {
      * Fix will be in 2.3.4, but not released yet (as of 2014-Jul-10).
      */
     @JsonProperty
-    void setServers(List<HostAndPort> servers) {
+    void setServers(HostAndPort[] servers) {
+        checkNotNull(servers);
+
         _servers = FluentIterable
-                .from(servers)
+                .from(Arrays.asList(servers))
                 .transform(new Function<HostAndPort, InetSocketAddress>() {
                     public InetSocketAddress apply(HostAndPort input) {
                         return new InetSocketAddress(input.getHostText(), input.getPort());
