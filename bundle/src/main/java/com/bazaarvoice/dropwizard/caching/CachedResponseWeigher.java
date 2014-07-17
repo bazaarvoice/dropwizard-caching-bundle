296cc86b7742bbcd81c6ef3c68d0becf232ab489
Cory Thomas <cory.thomas@bazaarvoice.com>
2014-07-17 16:56:25 -0500

    More accurate weight calculation

--- bundle/src/main/java/com/bazaarvoice/dropwizard/caching/CachedResponseWeigher.java
+++ bundle/src/main/java/com/bazaarvoice/dropwizard/caching/CachedResponseWeigher.java
@@ -1,13 +1,16 @@
 package com.bazaarvoice.dropwizard.caching;
 
-import com.google.common.base.Optional;
 import com.google.common.cache.Weigher;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Weigher that attempts to estimate the size of the cached response in bytes.
  */
-public class CachedResponseWeigher implements Weigher<String, Optional<CachedResponse>> {
-    private static final int HEADER_OVERHEAD_BYTES = 100;
+public class CachedResponseWeigher implements Weigher<String, CachedResponse> {
+    private static final int CHAR_BYTES = 2;
     public static final CachedResponseWeigher INSTANCE = new CachedResponseWeigher();
 
     private CachedResponseWeigher() {
@@ -15,10 +18,26 @@ public class CachedResponseWeigher implements Weigher<String, Optional<CachedRes
     }
 
     @Override
-    public int weigh(String key, Optional<CachedResponse> value) {
-        return (key.length() * 2) +
-                (value.isPresent()
-                        ? value.get().getResponseContent().length + HEADER_OVERHEAD_BYTES
-                        : 0);
+    public int weigh(@Nonnull String key, @Nonnull CachedResponse value) {
+        int weight = 0;
+
+        // This just an estimate for weighing purposes, not a precise calculation of memory use.
+
+        // Size of the key
+        weight += key.length() * CHAR_BYTES;
+
+        // Size of the headers
+        for (Map.Entry<String, List<String>> header : value.getResponseHeaders().entrySet()) {
+            weight += header.getKey().length() * CHAR_BYTES;
+
+            for (String headerValue : header.getValue()) {
+                weight += headerValue.length() * CHAR_BYTES;
+            }
+        }
+
+        // Size of the body
+        weight += value.getResponseContent().length;
+
+        return weight;
     }
 }
