d2196915293dab4c8faf9f6cdef7de4f73850754
Cory Thomas <cory.thomas@bazaarvoice.com>
2014-07-22 12:17:39 -0500

    Tests for MemcachedResponseStore

--- bundle/src/test/java/com/bazaarvoice/dropwizard/caching/memcached/MemcachedResponseStoreTest.java
+++ bundle/src/test/java/com/bazaarvoice/dropwizard/caching/memcached/MemcachedResponseStoreTest.java
@@ -1,12 +1,23 @@
 package com.bazaarvoice.dropwizard.caching.memcached;
 
+import com.bazaarvoice.dropwizard.caching.CachedResponse;
+import com.google.common.base.Optional;
 import net.spy.memcached.MemcachedClient;
+import net.spy.memcached.transcoders.Transcoder;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.same;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertSame;
 
 /**
  * Tests for {@link MemcachedResponseStore}.
@@ -16,9 +27,9 @@ public class MemcachedResponseStoreTest {
     public static Object[][] invalidateData() {
         return new Object[][]{
                 {"", false, "key", "key"},
-                {"", true, "key", null},
+                {"", true, "key", "key"},
                 {"pre", false, "key", "prekey"},
-                {"pre", true, "key", null}
+                {"pre", true, "key", "prekey"}
         };
     }
 
@@ -28,10 +39,80 @@ public class MemcachedResponseStoreTest {
         MemcachedResponseStore store = new MemcachedResponseStore(client, prefix, readOnly);
         store.invalidate(key);
 
-        if (memcacheKey != null) {
+        if (!readOnly) {
             verify(client).delete(memcacheKey);
         }
 
         verifyNoMoreInteractions(client);
+    }
+
+    @DataProvider
+    public static Object[][] putData() {
+        return new Object[][]{
+                {"", false, "key", "key", 1388534400, response(new DateTime(2014, 1, 1, 0, 0, 0, 0, DateTimeZone.UTC))},
+                {"", true, "key", "key", 1388534400, response(new DateTime(2014, 1, 1, 0, 0, 0, 0, DateTimeZone.UTC))},
+                {"pre", false, "key", "prekey", 1388534400, response(new DateTime(2014, 1, 1, 0, 0, 0, 0, DateTimeZone.UTC))},
+                {"pre", true, "key", "prekey", 1388534400, response(new DateTime(2014, 1, 1, 0, 0, 0, 0, DateTimeZone.UTC))}
+        };
+    }
+
+    @Test(dataProvider = "putData")
+    public void put(String prefix, boolean readOnly, String key, String memcacheKey, int memcacheExpiration, CachedResponse value) {
+        MemcachedClient client = mock(MemcachedClient.class);
+        MemcachedResponseStore store = new MemcachedResponseStore(client, prefix, readOnly);
+        store.put(key, value);
+
+        if (!readOnly) {
+            verify(client).set(eq(memcacheKey), eq(memcacheExpiration), same(value), any(Transcoder.class));
+        }
+
+        verifyNoMoreInteractions(client);
+    }
+
+    @Test
+    public void put_with_no_expiration() {
+        MemcachedClient client = mock(MemcachedClient.class);
+        MemcachedResponseStore store = new MemcachedResponseStore(client, "", false);
+        store.put("key", response(null));
+        verifyNoMoreInteractions(client);
+    }
+
+    @DataProvider
+    public static Object[][] getData() {
+        return new Object[][]{
+                {"", false, "key", "key", null},
+                {"", true, "key", "key", null},
+                {"pre", false, "key", "prekey", null},
+                {"pre", true, "key", "prekey", null},
+
+                {"", false, "key", "key", mock(CachedResponse.class)},
+                {"", true, "key", "key", mock(CachedResponse.class)},
+                {"pre", false, "key", "prekey", mock(CachedResponse.class)},
+                {"pre", true, "key", "prekey", mock(CachedResponse.class)}
+        };
+    }
+
+    @Test(dataProvider = "getData")
+    public void get(String prefix, boolean readOnly, String key, String memcacheKey, CachedResponse response) {
+        MemcachedClient client = mock(MemcachedClient.class);
+        MemcachedResponseStore store = new MemcachedResponseStore(client, prefix, readOnly);
+
+        when(client.get(eq(memcacheKey), any(Transcoder.class))).thenReturn(response);
+
+        Optional<CachedResponse> getResponse = store.get(key);
+
+        if (response == null) {
+            assertFalse(getResponse.isPresent());
+        } else {
+            assertSame(getResponse.get(), response);
+        }
+
+        verify(client).get(eq(memcacheKey), any(Transcoder.class));
+    }
+
+    private static CachedResponse response(DateTime expires) {
+        CachedResponse response = mock(CachedResponse.class);
+        when(response.getExpires()).thenReturn(Optional.fromNullable(expires));
+        return response;
     }
 }
